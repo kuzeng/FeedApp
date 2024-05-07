dd56eed789386fb4583fca3bceaa564fe66b7089
--replace-all <kuiduanz@gmail.com>
2024-05-06 23:08:56 -0700

    Initial Commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Kuiduan Zeng on 5/6/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
