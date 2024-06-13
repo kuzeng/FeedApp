e70e293484597730aa1aaa4ee0f038f79fc1e9dc
--replace-all <kuiduanz@gmail.com>
2024-06-13 13:35:46 -0700

    `CoreDataFeedStore.insert()` overrides previously inserted cache values

--- EssentialFeed/Feed Cache/ManagedCache+CoreDataProperties.swift
+++ EssentialFeed/Feed Cache/ManagedCache+CoreDataProperties.swift
@@ -25,6 +25,11 @@ extension ManagedCache {
         return try context.fetch(request).first
     }
     
+    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+        try find(in: context).map(context.delete)
+        return ManagedCache(context: context)
+    }
+    
     var localFeed: [LocalFeedImage] {
         return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
     }
