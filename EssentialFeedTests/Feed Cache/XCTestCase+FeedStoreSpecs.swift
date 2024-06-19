ef1b8571635b78918b248c12a612fad2a620639f
--replace-all <kuiduanz@gmail.com>
2024-06-18 17:17:59 -0700

    Simplify `URLSessionHTTPClient` completion code with the new `Result` APIs

--- EssentialFeedTests/Feed Cache/XCTestCase+FeedStoreSpecs.swift
+++ EssentialFeedTests/Feed Cache/XCTestCase+FeedStoreSpecs.swift
@@ -139,7 +139,7 @@ extension FeedStoreSpecs where Self: XCTestCase {
             
             exp.fulfill()
         }
-        wait(for: [exp], timeout: 2.0)
+        wait(for: [exp], timeout: 3.0)
         return deletionError
     }
     
