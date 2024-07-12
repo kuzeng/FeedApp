a6e3ddaf81a356fb323720847c6ae93d0f5df7ac
kuzeng <kuiduanz@gmail.com>
2024-07-12 14:54:24 -0700

    `RemoteFeedImageDataLoader` does not perform any URL request

---
+++ EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -0,0 +1,12 @@
+//
+//  RemoteFeedImageDataLoader.swift
+//  EssentialFeed
+//
+//  Created by Kuiduan Zeng on 7/12/24.
+//
+
+import Foundation
+
+public final class RemoteFeedImageDataLoader {
+
+}
